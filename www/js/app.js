5b66d3d08548dde8c71ac0eeed01da34a7fb466d
jeffreykong <jeffreykong@pcp134350pcs.wireless.calpoly.edu>
2015-02-28 15:36:02 -0800

    jumping player

--- www/js/app.js
+++ www/js/app.js
@@ -49,7 +49,6 @@
                 // Set the player's X velocity
 
                 player.velX += player.runSpeed; // If touching, runSpeed is positive, otherwise smaller negative
-
                 // Limit velX 0-maxSpeed
                 if (player.velX < 0) {
                     player.velX = 0;
@@ -64,7 +63,7 @@
 
 
                 // Set the player's Y velocity
-                player.velY += gravity;
+                player.velY += 1;
                 if (player.velY > player.maxYSpeed) {
                     player.velY = player.maxYSpeed;
                 }
@@ -87,12 +86,12 @@
                 if (player.y + player.h > canvas.height) {
                         console.log("Player hit bottom edge of screen");
                         player.y = canvas.height - player.h;
-                        player.velY = 0;
+                    //    player.velY = 0;
                 }
                 if (player.y < 0) {
                         console.log("Player hit top edge of screen");
                         player.y = 0;
-                        player.velY *= -0.75;
+                    //    player.velY *= -0.75;
                 }
             },
             
@@ -169,10 +168,11 @@
             player.runSpeed = -5;
             end = new Date();
             player.tapLength = end.getTime()-start.getTime();
+
             if(player.tapLength < 300){
                 // do tap things: jump, attack, grapple, etc.
                 //console.log("TAP! ");
-
+                player.velY = -20;
             }            
         }
         
